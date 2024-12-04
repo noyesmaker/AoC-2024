4226ec6df8a67d6cadc6e049fbbdf3d27b2045b1
Matthew Noyes <185992724+noyesmaker@users.noreply.github.com>
2024-12-04 00:36:20 -0600

    :sparkles: Add 04 Part 2 solution

--- 04/solve.py
+++ 04/solve.py
@@ -1,3 +1,4 @@
+#region Part 1
 def horizontal_f(matrix, i, j):
     retval = 0
 
@@ -50,7 +51,30 @@ def diagonal_bl(matrix, i, j):
     if (len(matrix) - i >= 4) and (j >= 3) and (matrix[i][j] + matrix[i+1][j-1] + matrix[i+2][j-2] + matrix[i+3][j-3] == "XMAS"):
         retval = 1
     return retval
+#endregion
 
+#region Part 2
+def xmas(matrix, i, j):
+    retval = 0
+
+    if (i >= 1) and (j >= 1) and (len(matrix) - i >= 2) and (len(matrix[i]) - j >= 2) and (matrix[i][j] == 'A'):
+        
+        tl = matrix[i-1][j-1]
+        br = matrix[i+1][j+1]
+        tr = matrix[i-1][j+1]
+        bl = matrix[i+1][j-1]
+
+        print(tl,'.',tr)
+        print('.','A','.')
+        print(bl, '.', br)
+
+        if ((tl == 'M' and br == 'S') or (tl == 'S' and br == 'M')) and \
+           ((tr == 'M' and bl == 'S') or (tr == 'S' and bl == 'M')):
+            print("match!")
+            retval = 1
+
+    return retval
+#endregion
 with open("input.txt") as input:
     grid = []
 
@@ -60,14 +84,15 @@ with open("input.txt") as input:
     
     sumxmas = 0
     sumcrossmas = 0
-    
+
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            sum += horizontal_f(grid, i, j) + horizontal_b(grid, i, j) + \
+            sumxmas += horizontal_f(grid, i, j) + horizontal_b(grid, i, j) + \
                    vertical_f(grid, i, j) + vertical_b(grid, i, j) + \
                     diagonal_tl(grid, i, j) + diagonal_tr(grid, i,j) + \
                     diagonal_bl(grid, i, j) + diagonal_br(grid, i,j)
-                    
+            
+            sumcrossmas += xmas(grid, i, j)
     
     print("Sum XMAS:", sumxmas)
     print("Sum CROSSMAS:", sumcrossmas)
